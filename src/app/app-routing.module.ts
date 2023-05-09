c484454a7f07079cf4364854244676cfc23b349d
DESKTOP-UNO <freddy.j.lozano@gmail.com>
2023-05-09 01:20:13 -0400

    Added all pages.
    All images seem to be showing up. Not all links tested yet.
    
    Automatic Gallery for testimonials now broken, html javascript not supported. Will need to fix.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CommonModule} from "@angular/common";
 
-const routes: Routes = [];
+//pages
+import{HomeComponent} from "./components/home/home.component";
+import{AboutComponent} from "./components/about/about.component";
+import{ServicesComponent} from "./components/services/services.component";
+import{ContactComponent} from "./components/contact/contact.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
